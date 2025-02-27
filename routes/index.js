b8392f90b2cd55bf3da979c40508e94fccf7a681
pswincher <preston.swincher@gmail.com>
2025-02-26 18:40:44 -0800

    router / fix

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,14 @@ const gamesRouter = require("./games");
 const routeNotFound = require("./routeNotFound");
 const { auth } = require("../middlewares/auth");
 
+router.get("/", (req, res) => {
+  res.json({ message: "API is running" });
+});
+
 router.use("/users", auth, userRouter);
 router.use("/games", gamesRouter);
 router.use("/signin", signinRouter);
 router.use("/signup", signUpRouter);
-router.use("/", gamesRouter);
+router.use("/", routeNotFound);
 
 module.exports = router;
